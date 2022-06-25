1dbc7ef7557083162fbc3642e3683bc56097f4b8
leonardo agustin romero <leo123leo@live.com.ar>
2022-06-25 01:14:33 -0300

    comint 10

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import React from 'react';
 import { isDOMComponent } from 'react-dom/test-utils';
 import NarBar from './components/NavBar';
@@ -9,23 +9,7 @@ import NarBar from './components/NavBar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <NarBar />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-
-
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NarBar />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,29 @@ const NarBar = ()=>{
 
     return(
     <div>
-        NARBAR
+        <header>
+            <img src="../fotos/logo.ico" alt="logo" className="header__logo"/>
+        <h1 className="header__h1"><b>Che Que Rico</b></h1>
+        <nav className="header__nav">
+            <ul className="header__nav__ul">
+                <li className="header__nav__ul--li">
+                    <a className="header__nav__ul--li--a" href="index.html">HOME</a>
+                </li>
+                <li className="header__nav__ul--li">
+                    <a className="header__nav__ul--li--a" href="imagenes.html">IMAGENES</a>
+                </li>
+                <li className="header__nav__ul--li">
+                    <a className="header__nav__ul--li--a" href="nosotros.html">NOSOTROS</a>
+                </li>
+                <li className="header__nav__ul--li">
+                    <a className="header__nav__ul--li--a" href="producto.html">PRODUCTOS</a>
+                </li>
+                <li className="header__nav__ul--li">
+                    <a className="header__nav__ul--li--a" href="contacto.html">CONTACTO</a>
+                </li>
+            </ul>
+        </nav>
+        </header>
     </div>)
 }
 
