360c073e878ee916cae236ece1f4436bcf6eb7fd
leonardo agustin romero <leo123leo@live.com.ar>
2022-07-05 00:41:20 -0300

    53

--- src/components/containers/promesa.js
+++ src/components/containers/promesa.js
@@ -1,33 +1,45 @@
 import React from 'react'
+import { productos } from './datos';
 
 const Task = new Promise( (resolve, reject) => {
     let condicion = true
     if(condicion) {
-        resolve ('200 promesa ok')
+        resolve (productos)
     }else{
         reject ('404 promesa error')
     }
-    
-
-
 });
 
-setTimeout(() => {
-    
-}, 3000);
 
 const Promesa = () => {
-    Task
-    .then((results) => {
-        //throw new Error('se rompio todo') // error se rompio todo
-        //console.log (results)
-        return results;
-    })
-    .catch((err) => console.log (err))
-    .then((res) => {
-        console.log (res)
-    })
-    .finally(() => console.log('finalizado'))
+    setTimeout(() => {
+        Task
+        .then((results) => {
+            //throw new Error('se rompio todo') // error se rompio todo
+            //console.log (results)
+            return results;
+        })
+        .catch((err) => console.log (err))
+        .then((res) => {
+            console.log (res)
+        })
+        .finally(() => console.log('finalizado'))
+        
+        console.log("productos simulacion 3 segundos")
+    }, 3000);
+    setTimeout(() => {
+        Task
+        .then((results) => {
+            return results;
+        })
+        .catch((err) => console.log (err))
+        .then((res) => {
+            console.log (res)
+        })
+        .finally(() => console.log('finalizado'))
+
+        console.log("producto simulacion 6 segundos")
+    }, 6000);
 
     return (
         <> 
